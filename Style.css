8e9101d3e4b2c6347a1b9b77bd1f22b9c4f47f29
romi-oss <hashjunior9@gmail.com>
2026-05-17 08:37:05 +0300

    Add initial styles for game layout and elements

---
+++ Style.css
@@ -0,0 +1,37 @@
+body {
+  text-align: center;
+  background: black;
+  color: white;
+}
+
+#game {
+  position: relative;
+  width: 800px;
+  height: 400px;
+  background: #111;
+  margin: auto;
+  border: 2px solid white;
+}
+
+#ball {
+  position: absolute;
+  width: 15px;
+  height: 15px;
+  background: white;
+  border-radius: 50%;
+}
+
+#playerPaddle, #computerPaddle {
+  position: absolute;
+  width: 10px;
+  height: 80px;
+  background: white;
+}
+
+#playerPaddle {
+  left: 10px;
+}
+
+#computerPaddle {
+  right: 10px;
+}
